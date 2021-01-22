ca2b7967fc6eb04b7ee43de8e5f550d386831cab
alex <luquankhanh68@gmail.com>
2021-01-22 07:53:25 -0800

    init project

---
+++ src/server.ts
@@ -0,0 +1,17 @@
+import https from 'https';
+import fs from 'fs';
+import app from './app';
+
+const port: number = 3000;
+const key: Buffer = fs.readFileSync(__dirname + '/../certs/selfsigned.key');
+const cert: Buffer = fs.readFileSync(__dirname + '/../certs/selfsigned.crt');
+const options: Record<string, Buffer> = {
+    key,
+    cert,
+};
+
+const server = https.createServer(options, app);
+
+server.listen(port, () => {
+    console.log('server starting on port : ' + port);
+});
