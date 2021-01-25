1cc291813223966a4bd21dadaae37e25a222bc5d
alex <luquankhanh68@gmail.com>
2021-01-25 14:42:05 -0800

    fix mock

--- src/unsplash/routes/search.ts
+++ src/unsplash/routes/search.ts
@@ -12,16 +12,44 @@ const router: Router = Router();
 router.get(
     '/search',
     async (req: Request, res: Response, next: NextFunction) => {
-        console.log(req.query.q);
-        // const result: ApiResponse<Photos> = await unsplash.search.getPhotos({
-        //     query: req.query.q as string,
-        //     perPage: 30,
-        //     // orientation: 'portrait',
-        //     // orientation: 'landscape',
-        //     orientation: 'squarish',
+        console.log(req.query);
+        const page: number = +(req.query.page as string);
+        // try {
+        //     const result: ApiResponse<Photos> = await unsplash.search.getPhotos(
+        //         {
+        //             query: req.query.q as string,
+        //             perPage: 30,
+        //             // orientation: 'portrait',
+        //             // orientation: 'landscape',
+        //             orientation: 'squarish',
+        //         },
+        //         { signal }
+        //     );
+        //     res.status(200).send(result.response);
+        // } catch (err: any) {
+        //     res.status(500).send('message': 'An error is occured in unsplash api');
+        // }
+        const results: Array<any> = [
+            squarishData.results,
+            portraitData.results,
+            landscapeData.results,
+            testData.results,
+        ];
+        if (page <= results.length) {
+            res.status(200).send({
+                results: [...results[page - 1]],
+            });
+        } else {
+            res.status(500).send({
+                message: 'An error is occured in unsplash api',
+            });
+        }
+        // const downloadLink: ApiResponse<{
+        //     url: string;
+        // }> = await unsplash.photos.trackDownload({
+        //     downloadLocation: testData.results[0].links.download_location,
         // });
-        // res.status(200).send(result.response);
-        res.status(200).send(squarishData);
+        // console.log(downloadLink);
     }
 );
 
