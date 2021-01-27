f569e4bc8995f4355e716e05836d0e82b96747a8
alex <luquankhanh68@gmail.com>
2021-01-27 15:44:04 -0800

    calculate canvas size based on image's width and height

--- src/unsplash/routes/search.ts
+++ src/unsplash/routes/search.ts
@@ -41,19 +41,25 @@ router.get(
                     const dice: number = Math.floor(Math.random() * 20);
                     let width: number = 1080;
                     let height: number = 1080;
+                    let canvasWidth: number = 64;
+                    let canvasHeight: number = 64;
                     let classSize: string = 'row-span-2 col-span-2';
                     let ar: string = '1:1';
 
                     if (dice <= 5) {
                         width /= 2;
                         height /= 2;
+                        canvasWidth /= 2;
+                        canvasHeight /= 2;
                         classSize = '';
                     } else if (dice > 5 && dice <= 10) {
                         height /= 2;
+                        canvasHeight /=2;
                         classSize = 'col-span-2';
                         ar = '2:1';
                     } else if (dice > 10 && dice <= 15) {
                         width /= 2;
+                        canvasWidth /= 2;
                         classSize = 'row-span-2';
                         ar = '1:2';
                     }
@@ -73,6 +79,10 @@ router.get(
                             ].join('&'),
                         },
                         classSize,
+                        width,
+                        height,
+                        canvasWidth,
+                        canvasHeight
                     };
                 }
             );
